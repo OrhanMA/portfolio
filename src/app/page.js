dc1a63a51db09e4d4f9810ae7f14ec7d431d2bde
Orhan <orhan.madi.assani@gmail.com>
2023-12-03 17:52:05 +0100

    under construciton

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,9 @@ import React from "react";
 
 function Page() {
   return (
-    <div>
-      <h3 className="text-2xl md:text-3xl lg:text-4xl font-semibold">
-        Hi, my name is Orhan and I&apos;m a{" "}
-        <span className="text-violet-400">Front-End</span> developer based in
-        Chambery, France. <br /> I&apos;m{" "}
-        <span className="text-violet-400 underline">
-          currently looking for a junior position
-        </span>{" "}
-        to start off my career in web development.
-      </h3>
+    <div className="flex flex-col gap-12 justify-center items-center h-screen w-screen bg-black text-white">
+      <h1 className="text-4xl">En cours de developpement</h1>
+      <h2 className="text-3xl">Revenez plust tard...</h2>
     </div>
   );
 }
