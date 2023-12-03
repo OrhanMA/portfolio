77418fc3e4cec6058e2ab1695c1903d369f4af12
Orhan <orhan.madi.assani@gmail.com>
2023-12-03 18:07:58 +0100

    correction ortho

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import React from "react";
 function Page() {
   return (
     <div className="flex flex-col gap-12 justify-center items-center h-screen w-screen bg-black text-white">
-      <h1 className="text-4xl">En cours de developpement</h1>
-      <h2 className="text-3xl">Revenez plust tard...</h2>
+      <h1 className="text-4xl">En cours de développement</h1>
+      <h2 className="text-3xl">Revenez plus tard...</h2>
     </div>
   );
 }
