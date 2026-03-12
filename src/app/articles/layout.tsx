00af078a1e9439e2a249abb86936664af5db9a69
OrhanMA <orhan.madi.assani@gmail.com>
2026-03-12 11:12:39 +0100

    feat: initial commit

---
+++ src/app/articles/layout.tsx
@@ -0,0 +1,11 @@
+export default function ArticlesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="mx-auto max-w-3xl px-6 py-16 prose dark:prose-invert">
+      {children}
+    </div>
+  );
+}
